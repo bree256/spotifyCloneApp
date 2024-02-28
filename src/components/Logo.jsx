daa192e07d9405e18cb95936400a1b7e630b6a3a
bree256 <141923968+bree256@users.noreply.github.com>
2024-02-28 20:32:46 +0300

    Added the logo component

---
+++ src/components/Logo.jsx
@@ -0,0 +1,6 @@
+import spotifywhite from "../assets/images/spotifywhite.svg";
+import spotifyblack from "../assets/images/spotifyblack.svg";
+
+export function Logo() {
+  return <img src={spotifywhite} alt="spotifylogo" />;
+}
