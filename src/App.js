3b75664b78a9f888375228f20b08495f04426be1
bree256 <141923968+bree256@users.noreply.github.com>
2024-02-29 22:15:21 +0300

    Added a Blue component section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import LandingPageHeader from "./components/LandingPageHeader";
+import BlueSection from "./components/BlueSection";
 function App() {
   return (
     <>
       <LandingPageHeader />
+      <BlueSection />
     </>
   );
 }
