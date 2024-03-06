191ecd4859c62c9d519b2d25b5c23297bb567629
bree256 <141923968+bree256@users.noreply.github.com>
2024-03-06 16:35:09 +0300

    added the Hero section

--- src/components/ButtonsBlueSection.jsx
+++
@@ -1,9 +0,0 @@
-// import ButtonsBlueSection from "../components/ButtonsBlueSection";
-import "../components/BlueSection.css";
-export function ButtonsBlueSection({ backgroundColor, text, border }) {
-  return (
-    <button style={{ backgroundColor: backgroundColor, border: border }}>
-      {text}
-    </button>
-  );
-}
--- src/components/Logo.jsx
+++
@@ -1,6 +0,0 @@
-import spotifywhite from "../assets/images/spotifywhite.svg";
-import spotifyblack from "../assets/images/spotifyblack.svg";
-
-export function Logo() {
-  return <img src={spotifywhite} alt="spotifylogo" />;
-}
